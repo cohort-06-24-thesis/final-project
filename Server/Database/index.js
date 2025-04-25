c067fad7fad51fb7b338a42669272408105f2032
RayenDamdoum <rdamdoum@gmail.com>
2025-04-25 15:33:36 +0100

    user table  done

--- Server/Database/index.js
+++ Server/Database/index.js
@@ -1,9 +1,13 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize,DataTypes } = require('sequelize')
 
 const sequelize = new Sequelize('donation', 'postgres', 'root', {
     host: 'localhost',
     dialect:'postgres'
   });
+
+
+
+  const User = require('../models/user.model')(sequelize,DataTypes);
 
   try {
    sequelize.authenticate();
@@ -11,4 +15,10 @@ const sequelize = new Sequelize('donation', 'postgres', 'root', {
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
+// const connection=async()=>{
+//   await sequelize.sync({ force: true });
+// console.log('All models were synchronized successfully.');
+// }
+// connection()
 
+module.exports={User}
