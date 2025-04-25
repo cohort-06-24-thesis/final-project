c05b4526018843d2df0c7dd1527a30020a8e3c5e
RayenDamdoum <rdamdoum@gmail.com>
2025-04-25 15:24:08 +0100

    database connection done

---
+++ Server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require("./Database/index.js")
+
+
+const app=express();
+const Port=3000;
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors()) 
+
+
+// Start the server
+app.listen(Port, () => {
+    console.log(`Server is running on http://localhost:${Port}`);
+  });
+
+
+
+
