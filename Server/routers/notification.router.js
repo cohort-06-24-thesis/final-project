d0a75f5dc2a330180f6e31d5b0e4bdc19aa20f5c
Benali-mouin <benalimouin3@gmail.com>
2025-04-27 15:56:15 +0100

    notification is done

---
+++ Server/routers/notification.router.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const {}= require("../controllers/notification.controller");
+const Router = express.Router();
+
+Router.post("/Addnotification",createNotif);
+Router.get("/GetAllnotification",findAllNotif);
+Router.get("/GetOnenotification/:id",findOneNotif);
+Router.put("/Updatenotification/:id",updateNotif);
+Router.delete("/Deletenotification/:id",deleteNotif);
+
+module.exports = Router;
--- Server/routers/payment.router.js
+++ Server/routers/payment.router.js
@@ -4,7 +4,7 @@ const Router = express.Router();
 
 
 Router.post("/payment",Add);
-Router.post("/payment/:id",verify)
+Router.post("/verify/:id",verify)
 
 
 
