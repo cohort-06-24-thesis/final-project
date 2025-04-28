014faf52fabe1af504e69ceb24818f3d0f7ae220
RayenDamdoum <rdamdoum@gmail.com>
2025-04-28 16:41:56 +0100

    landing pages

--- Client/App.js
+++ Client/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import LandingPage from './screens/LandingPage';
 
 export default function App() {
+  const [isFirstTime, setIsFirstTime] = useState(true);
+
+  if (isFirstTime) {
+    return <LandingPage onFinish={() => setIsFirstTime(false)} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hi m3ana rabbi
-        twakkalna 3la allah  </Text>
-     
+      <Text>Hi m3ana rabbi </Text>
       <StatusBar style="auto" />
     </View>
   );
