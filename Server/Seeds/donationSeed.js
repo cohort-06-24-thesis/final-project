e0aca9c4526964da3558c07d94c1f7d19d579489
RayenDamdoum <rdamdoum@gmail.com>
2025-05-26 16:03:19 +0100

    multiple images doantions +in need done

--- Server/Seeds/donationSeed.js
+++ Server/Seeds/donationSeed.js
@@ -32,7 +32,7 @@ const seedDonationItems = async () => {
         description: 'Warm clothes for children in need, including jackets, sweaters, and gloves.',
         image: [
           'https://www.veteransplaceusa.org/wp-content/uploads/2019/12/Clothes-Donation-1024x683.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Clothing-Donation-1.jpg'
+          'https://www.bathecho.co.uk/uploads/2022/03/pile-clothes-donation-ss.jpg'
         ],
         status: 'available',
         location: 'Tunis',
@@ -47,7 +47,7 @@ const seedDonationItems = async () => {
         description: 'New notebooks, pens, pencils, and backpacks for underprivileged students.',
         image: [
           'https://i.ebayimg.com/images/g/Bv4AAOSwHSxjBXH3/s-l1200.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/School-Supplies-Donation-1.jpg'
+          'https://www.colonycards.com/cdn/shop/files/school_suppliesphoto_983e52f1-50f0-424c-b93f-1224b57212c3_1024x1024.jpg?v=1716139610'
         ],
         status: 'reserved',
         location: 'Sfax',
@@ -62,7 +62,7 @@ const seedDonationItems = async () => {
         description: 'Essential food items including rice, pasta, canned goods, and cooking oil.',
         image: [
           'https://5.imimg.com/data5/TJ/FA/XL/SELLER-31929298/packaging-services-for-food-items.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Food-Donation-1.jpg'
+          'https://c8.alamy.com/comp/G1PG10/arrangement-of-various-types-of-food-packaging-G1PG10.jpg'
         ],
         status: 'claimed',
         location: 'Sousse',
@@ -77,7 +77,7 @@ const seedDonationItems = async () => {
         description: 'First aid kits, bandages, and basic medical equipment for local clinics.',
         image: [
           'https://doctoressentials.co.uk/cdn/shop/articles/xdoctor-essentials-medical-supplies-uk.jpg.pagespeed.ic.BQ2XIBfVAc.webp?v=1715289266&width=1280',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Medical-Supplies-Donation-1.jpg'
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYe6Hn3MlrIQS8XO5efeu8vgkowjAxWgv2Tg&s'
         ],
         status: 'available',
         location: 'Bizerte',
@@ -92,7 +92,7 @@ const seedDonationItems = async () => {
         description: 'Educational books and children\'s storybooks in good condition.',
         image: [
           'https://www.elocalshops.com/cdn/shop/products/817dIguWe9L.jpg?v=1693770058',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Book-Donation-1.jpg'
+          'https://blogs.ucl.ac.uk/special-collections/files/2020/04/a-collection-of-books.jpg'
         ],
         status: 'available',
         location: 'Nabeul',
@@ -107,7 +107,7 @@ const seedDonationItems = async () => {
         description: 'Gently used furniture including tables, chairs, and cabinets.',
         image: [
           'https://5.imimg.com/data5/SELLER/Default/2023/9/344499330/AO/JT/HS/62046965/full-size-teak-wood-bed-500x500.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Furniture-Donation-1.jpg'
+          'https://www.saimfurniturefactory.pk/storage/2024/07/Bedroom-Furniture-Set-Price-2.webp'
         ],
         status: 'reserved',
         location: 'Monastir',
@@ -122,7 +122,7 @@ const seedDonationItems = async () => {
         description: 'New and gently used toys, board games, and educational materials.',
         image: [
           'https://www.uttamtoys.com/wp-content/uploads/2023/07/Toys-Games-Category-icon.png',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Toy-Donation-1.jpg'
+          'https://s7.orientaltrading.com/is/image/OrientalTrading/13942376-a01?$1x1main$&$NOWA$'
         ],
         status: 'available',
         location: 'Gabes',
@@ -138,7 +138,7 @@ const seedDonationItems = async () => {
         description: 'Personal hygiene items including soap, shampoo, toothbrushes, and sanitary products.',
         image: [
           'https://assets.aboutamazon.com/dims4/default/3f9d820/2147483647/strip/true/crop/1220x686+0+0/resize/1220x686!/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2Fe7%2F21%2F6eb107654cfe8f6f96c1eebb8ab1%2Fukraine-hygiene-kit-amazon.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Hygiene-Kit-Donation-1.jpg'
+          'https://www.sfsimplified.com/content/images/2024/06/A08A6172-45DE-4A50-91F1-09DC11C5C84D.JPG'
         ],
         status: 'available',
         location: 'Kairouan',
@@ -154,7 +154,7 @@ const seedDonationItems = async () => {
         description: 'Electric heaters for families in need during the winter season.',
         image: [
           'https://spacenet.tn/113074-large_default/chauffage-electrique-home-electronics-1200-watt-gris-tchh12bs.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Heater-Donation-1.jpg'
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDh4rCMZYQ0EzL2ct2uCoBJQ1e_VsgXnHJQbooKaod2Nqz37jFQ_1yjae90y5Iygp6I7I&usqp=CAU'
         ],
         status: 'reserved',
         location: 'Mahdia',
@@ -170,7 +170,7 @@ const seedDonationItems = async () => {
         description: 'Balls, rackets, and other sports gear for youth programs.',
         image: [
           'https://img.freepik.com/premium-photo/set-different-sports-equipment-white-background_495423-47354.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Sports-Equipment-Donation-1.jpg'
+          'https://www.bombaykidscompany.com/cdn/shop/files/65908_1.jpg?v=1722947458&width=533'
         ],
         status: 'available',
         location: 'Zaghouan',
@@ -185,7 +185,7 @@ const seedDonationItems = async () => {
         description: 'Diapers, baby clothes, and feeding supplies for new mothers.',
         image: [
           'https://www.bundleofjoybaskets.com/cdn/shop/products/20160919_115739.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Baby-Supplies-Donation-1.jpg'
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKHMgzUv3yY0gyaRSrG9cCE66g5Go5R1TWHw&s'
         ],
         status: 'available',
         location: 'Ariana',
@@ -201,7 +201,7 @@ const seedDonationItems = async () => {
         description: 'Paints, brushes, canvases, and other art materials for creative workshops.',
         image: [
           'https://m.media-amazon.com/images/I/81BrCSk5T3L._AC_SL1500_.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Art-Supplies-Donation-1.jpg'
+          'https://cdn.pixabay.com/photo/2021/10/14/00/20/colored-pencils-6707815_640.jpg'
         ],
         status: 'reserved',
         location: 'Hammamet',
@@ -217,7 +217,7 @@ const seedDonationItems = async () => {
         description: 'Guitars, keyboards, and percussion instruments for music education.',
         image: [
           'https://www.tickit.co.uk/cdn/shop/files/TickiT_Percussion_Set_85101_A_2919ddff-5d5f-4ad1-bc38-2a7673c6235d.jpg?crop=center&height=1200&v=1736078986&width=1200',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Musical-Instruments-Donation-1.jpg'
+          'https://friendsofchambermusic.ca/fcm-wp/wp-content/uploads/2017/05/The-Hammer-Stradivarius.jpg'
         ],
         status: 'available',
         location: 'Djerba',
@@ -233,7 +233,8 @@ const seedDonationItems = async () => {
         description: 'Shovels, rakes, and seeds for community garden projects.',
         image: [
           'https://spicymoustache.com/wp-content/uploads/2024/10/gardening-tools-GA-cover-800x568-1.png',
-          'https://hudsongracesf.com/cdn/shop/products/Garden_Tools_Trio_Product.jpg?v=1747698072&width=1445'
+          'https://hudsongracesf.com/cdn/shop/products/Garden_Tools_Trio_Product.jpg?v=1747698072&width=1445',
+          'https://homesteadingfamily.com/wp-content/uploads/2022/05/Homestead-iron-tools_HF.jpg'
         ],
         status: 'available',
         location: 'Tozeur',
@@ -249,7 +250,7 @@ const seedDonationItems = async () => {
         description: 'Printers, paper, and stationery for educational institutions.',
         image: [
           'https://img.freepik.com/premium-photo/school-equipment-table_200402-858.jpg?semt=ais_hybrid&w=740',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Office-Supplies-Donation-1.jpg'
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_FpnoVgkX0b-iS8g--WjflaxQlpmkCPKTcA&s'
         ],
         status: 'reserved',
         location: 'Gafsa',
@@ -265,7 +266,7 @@ const seedDonationItems = async () => {
         description: 'Pots, pans, and kitchen utensils for community kitchens.',
         image: [
           'https://cdn.prod.website-files.com/660856e6a9a1ca9e3d9269ad/66841798befeb1986c7eb929_15%20Must-Have%20Kitchen%20Tools%20to%20Cook%20Like%20a%20Pro%20(1).webp',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Kitchen-Equipment-Donation-1.jpg'
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrbSXmduEr5LSoZ_s9UcVTFBHSGVxYsuvqXg&s'
         ],
         status: 'available',
         location: 'Kasserine',
@@ -281,7 +282,6 @@ const seedDonationItems = async () => {
         description: 'Bicycles for children and adults to promote eco-friendly transportation.',
         image: [
           'https://images.offerup.com/BhPx1TyK6awpTNLXi1En3NKq1r8=/250x250/af12/af1213572be843d4bed0f997174dc748.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Bicycle-Donation-1.jpg'
         ],
         status: 'available',
         location: 'Beja',
@@ -297,7 +297,7 @@ const seedDonationItems = async () => {
         description: 'Flashlights, batteries, and emergency supplies for disaster preparedness.',
         image: [
           'https://i.ebayimg.com/images/g/svMAAOSwOrxZwXyg/s-l1200.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Emergency-Kit-Donation-1.jpg'
+          'https://aarp.widen.net/content/waj0amhnsw/jpeg/20250320-.org-disaster-go-bag_-121_v2_D-curved.jpg?crop=true&anchor=84,65&q=80&color=ffffffff&u=lywnjt&w=1841&h=1058'
         ],
         status: 'reserved',
         location: 'Jendouba',
@@ -313,7 +313,7 @@ const seedDonationItems = async () => {
         description: 'Books and audio materials for language education programs.',
         image: [
           'https://m.media-amazon.com/images/I/91gmmI4J92L.jpg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Language-Learning-Materials-Donation-1.jpg'
+          'https://images.kaplanco.com/catalog/jumbo/63059_r_20.jpg'
         ],
         status: 'available',
         location: 'Tataouine',
@@ -329,7 +329,6 @@ const seedDonationItems = async () => {
         description: 'Collection of recycling containers for environmental initiatives.',
         image: [
           'https://i.redd.it/d7b9wd3bsejc1.jpeg',
-          'https://www.volunteerhq.org/blog/wp-content/uploads/2021/12/Recycling-Bins-Donation-1.jpg'
         ],
         status: 'available',
         location: 'Medenine',
